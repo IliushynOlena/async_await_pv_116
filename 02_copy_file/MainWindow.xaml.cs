f354b7c5d046e9afd29e672f43950100b039d142
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-03-17 20:53:33 +0200

    add materials and multi copi

--- 02_copy_file/MainWindow.xaml.cs
+++ 02_copy_file/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using Microsoft.WindowsAPICodePack.Dialogs;
 using PropertyChanged;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Net.Security;
@@ -43,15 +44,22 @@ namespace _02_copy_file
         }
         private async void CopyButtonClick(object sender, RoutedEventArgs e)
         {
-            // type 1 - using File class
+            
             string filename = Path.GetFileName(model.Source);
             string destFilePath = Path.Combine(model.Destination , filename);
-            //File.Copy(Source, destFilePath, true);
-            await CopyFileAsync(model.Source, destFilePath);    
+            CopyProcessInfo info = new CopyProcessInfo(filename);
+            model.AddProcesses(info);
+            await CopyFileAsync(model.Source, destFilePath, info);    
+            //add item to list 
+
             MessageBox.Show("Complited!!!");
         }
-        private Task CopyFileAsync(string src, string dest)
+        private Task CopyFileAsync(string src, string dest, CopyProcessInfo info)
         {
+            #region Type 1 Type 2
+            // type 1 - using File class
+            //File.Copy(Source, destFilePath, true);
+
             //return Task.Run(() =>
             //{
             //    // type 2- using FileStream class
@@ -72,12 +80,18 @@ namespace _02_copy_file
             //    } while (bytes > 0);
 
             //});
-            return FileTransferManager.CopyWithProgressAsync(src, dest, (pr) =>
+            #endregion
+            return FileTransferManager.CopyWithProgressAsync(src, dest, (progress) =>
             {
-                model.Progress = pr.Percentage;
-
+                model.Progress = progress.Percentage;
+                info.Percentage = progress.Percentage;
+                info.BytesPerSecond = progress.BytesPerSecond;/// 1024 / 1024;
             }, false);
         }
+        //void ProgressHandler(TransferProgress pr)
+        //{
+        //    model.Progress = pr.Percentage;
+        //}
         private void OpenSourceClick(object sender, RoutedEventArgs e)
         {
             OpenFileDialog dialog = new OpenFileDialog();
@@ -99,9 +113,32 @@ namespace _02_copy_file
     [AddINotifyPropertyChangedInterface]
     class ViewModel
     {
+        private ObservableCollection<CopyProcessInfo> processes;
+        public ViewModel()
+        {
+            processes = new ObservableCollection<CopyProcessInfo>();
+        }
+        public IEnumerable<CopyProcessInfo> Processes => processes;
         public string Source { get; set; }
         public string Destination { get; set; }
         public double Progress { get; set; }
         public bool IsWaiting => Progress == 0;
+        public void AddProcesses(CopyProcessInfo info)
+        {
+            processes.Add(info);
+        }
+    }
+    [AddINotifyPropertyChangedInterface]
+    class CopyProcessInfo
+    {
+        public CopyProcessInfo(string filename)
+        {
+            FileName = filename;
+        }
+        public string FileName { get; set; }
+        public double Percentage { get; set; }
+        public int PercentageInt => (int)Percentage;
+        public double BytesPerSecond { get; set; }
+        public double MegaBytesPerSecond => Math.Round(BytesPerSecond / 1024 / 1024, 2);
     }
 }
